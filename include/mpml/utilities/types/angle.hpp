e4386f432285a4dc94567aa4b54d013077288958
Allosker <allosker.pro@gmail.com>
2026-01-05 18:07:38 +0100

    Added methods: toRadians(degrees) and toDegrees(radians) alongside Angles and made those functions and all of Angle [[nodiscard]].

--- include/mpml/utilities/types/angle.hpp
+++ include/mpml/utilities/types/angle.hpp
@@ -9,40 +9,39 @@
 
 namespace mpml
 {
-	
+
+	[[nodiscard]] constexpr float toRadians(float degrees) noexcept
+	{
+		return degrees * constants::PI_F / 180.f;
+	}
+
+	[[nodiscard]] constexpr float toDegrees(float radians) noexcept
+	{
+		return radians * 180.f / constants::PI_F;
+	}
 	
 	struct Angle
 	{ 
-		static constexpr Angle fromDegrees(float degrees) noexcept
+		[[nodiscard]] static constexpr Angle fromDegrees(float degrees) noexcept
 		{
 			return Angle{ degrees * constants::PI_F / 180.f };
 		}
 
 
-		static constexpr Angle fromRadians(float radians) noexcept
+		[[nodiscard]] static constexpr Angle fromRadians(float radians) noexcept
 		{
 			return Angle{ radians };
 		}
 
 
-		constexpr float asDegrees() const noexcept
+		[[nodiscard]] constexpr float asDegrees() const noexcept
 		{
 			return angle * 180.f / constants::PI_F;
 		}
 
-		constexpr float asRadians() const noexcept
+		[[nodiscard]] constexpr float asRadians() const noexcept
 		{
 			return angle;
-		}
-
-		constexpr float toRadians(float degrees) const noexcept
-		{
-			return degrees * constants::PI_F / 180.f;
-		}
-
-		constexpr float toDegrees(float radians) const noexcept
-		{
-			return radians * 180.f / constants::PI_F;
 		}
 
 
@@ -58,12 +57,12 @@ namespace mpml
 	};
 
 
-	constexpr Angle operator""_rad(long double degrees)
+	[[nodiscard]] constexpr Angle operator""_rad(long double degrees)
 	{
 		return Angle::fromDegrees(static_cast<float>(degrees));
 	}
 
-	constexpr Angle operator""_deg(long double radians)
+	[[nodiscard]] constexpr Angle operator""_deg(long double radians)
 	{
 		return Angle::fromRadians(static_cast<float>(radians));
 	}
