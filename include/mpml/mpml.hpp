d9939038ea5705b7dfda43edfadeee3c70156fd0
Allosker <allosker.pro@gmail.com>
2026-04-12 20:01:15 +0200

    Fixed some matrices and added some functions -- added a lot of comments for myself.

--- include/mpml/mpml.hpp
+++ include/mpml/mpml.hpp
@@ -8,6 +8,8 @@
 * TODO LIST:
 *   - Make it so that the macro: MATRIX_LINEAR_PRINTING only activates via CMake
 *	- Make it so that the matrices stay consitent and all use the [][] system
+*	- Make a correct 2D orthographic projection <- Done (partially)
+*	- Remake the MAtrices so that they all follow the same mappin order
 */
 
 // Lib Files
